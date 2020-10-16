6c942cdc0cc620dfff5c114db18b8e65bebc7109
LinuxArchitects <heddar.abdelilah@live.fr>
2020-10-17 01:28:37 +0200

    Lab3 Lab4 Lab5

---
+++ Cassandra de a - z | 0405 LAB 4  Intéragir avec Cassandra/launch.sh
@@ -0,0 +1,5 @@
+wget https://downloads.apache.org/cassandra/4.0-beta2/apache-cassandra-4.0-beta2-bin.tar.gz 
+tar -xzf apache-cassandra-4.0-beta2-bin.tar.gz 
+mv apache-cassandra-4.0-beta2 /usr/share/cassandra 
+rm apache-cassandra-4.0-beta2-bin.tar.gz 
+export PATH="$PATH:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"
