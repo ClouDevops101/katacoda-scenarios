c3735949b0632498f975a5bb084ef27d994835ff
LinuxArchitects <heddar.abdelilah@live.fr>
2020-10-17 01:32:10 +0200

    Lab3 Lab4 Lab5

--- Cassandra de a - z | 0405 LAB 4  Intéragir avec Cassandra/background.sh
+++ Cassandra de a - z | 0405 LAB 4  Intéragir avec Cassandra/background.sh
@@ -1,7 +1,7 @@
 apt install openjdk-8-jre-headless -y
 JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-# wget https://downloads.apache.org/cassandra/4.0-beta2/apache-cassandra-4.0-beta2-bin.tar.gz 
-# tar -xzf apache-cassandra-4.0-beta2-bin.tar.gz 
-# mv apache-cassandra-4.0-beta2 /usr/share/cassandra 
-# rm apache-cassandra-4.0-beta2-bin.tar.gz 
-# export PATH="$PATH:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"
+wget https://downloads.apache.org/cassandra/4.0-beta2/apache-cassandra-4.0-beta2-bin.tar.gz 
+tar -xzf apache-cassandra-4.0-beta2-bin.tar.gz 
+mv apache-cassandra-4.0-beta2 /usr/share/cassandra 
+rm apache-cassandra-4.0-beta2-bin.tar.gz 
+export PATH="$PATH:/usr/share/cassandra/bin:/usr/share/cassandra/tools/bin"
