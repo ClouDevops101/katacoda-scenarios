f5929f575e36922fb2fdce74a1f83e04834b3b9c
LinuxArchitects <heddar.abdelilah@live.fr>
2020-10-17 02:00:05 +0200

    foreground scripts

--- Cassandra de a - z | 0405 LAB 4  Intéragir avec Cassandra/foreground.sh
+++ Cassandra de a - z | 0405 LAB 4  Intéragir avec Cassandra/foreground.sh
@@ -1,5 +1,5 @@
 #apt install openjdk-8-jre-headless -y
-JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 wget https://downloads.apache.org/cassandra/4.0-beta2/apache-cassandra-4.0-beta2-bin.tar.gz 
 tar -xzf apache-cassandra-4.0-beta2-bin.tar.gz 
 mv apache-cassandra-4.0-beta2 /usr/share/cassandra 
